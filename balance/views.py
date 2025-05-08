bdb547e7227e9c9548d0c818d306f5db0c6192b0
yecit1997 <ttorresyecit@gmail.com>
2025-05-08 00:13:04 -0500

    Agregamos el porsentaje de citas por día, juntamente con la grafica correspondiente

--- balance/views.py
+++ balance/views.py
@@ -1,24 +1,88 @@
 from django.shortcuts import render
-from django.contrib.auth.decorators import permission_required
-from django.contrib import messages
-from django.shortcuts import redirect, get_object_or_404
+from collections import Counter
+
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+import io
+import base64
 
 from cita.models import Cita
 
 def citas_por_mes(request):
     todas_las_citas = Cita.objects.all()
-    citas_por_mes = {}
+    nombres_meses = [cita.fecha.strftime('%b') for cita in todas_las_citas]
+    nombres_dias = [cita.fecha.strftime('%d') for cita in todas_las_citas]
+
+    conteo_meses = dict(Counter(nombres_meses))
+    conteo_dias = dict(Counter(nombres_dias))
     
-    for cita in todas_las_citas:
-        fecha = cita.fecha
-        mes = fecha.strftime('%B')  # Obtener el nombre del mes en español
-        anio = fecha.year
-        print(mes, anio)
-        if mes not in citas_por_mes:
-            citas_por_mes[mes] = {}
+    total_citas = len(todas_las_citas)
+    porcentaje_por_mes = {} # Inicializar el diccionario para almacenar los porcentajes
         
-        context = {
-            'citas_por_mes': citas_por_mes,
-            'todas_las_citas': todas_las_citas,
-        }
+    # Calcular el porcentaje de citas por mes
+    for mes, cantidad in conteo_meses.items():
+        porcentaje_por_mes[mes] = round((cantidad / total_citas) * 100, 2)
+
+    meses = list(porcentaje_por_mes.keys()) # Obtener los meses en el orden correcto
+    porcentajes_meses = list(porcentaje_por_mes.values()) # Obtener los porcentajes en el orden correcto
+    try:
+        # Crear el gráfico de barras con matplotlib
+        plt.figure(figsize=(5, 3))
+        plt.bar(meses, porcentajes_meses, color='skyblue', width=0.5)
+        plt.xlabel('Mes')
+        plt.ylabel('Porcentaje (%)')
+        plt.title('Porcentaje de Citas por Mes')
+        plt.ylim(0, 100)  # Asegurar que el eje Y vaya de 0 a 100
+        
+        # plt.pie(porcentajes, labels=meses, autopct='%1.1f%%', startangle=140)
+        
+        # Guardar el gráfico en un buffer de memoria
+        buffer = io.BytesIO()
+        plt.savefig(buffer, format='png')
+        buffer.seek(0)
+        grafico_porcentaje_meses = base64.b64encode(buffer.read()).decode('utf-8')
+        plt.close()
+    except:
+      print('Ocurrió un error al crear el gráfico.')
+      
+      
+    porcentaje_por_dia = {} # Inicializar el diccionario para almacenar los porcentajes
+    
+    # Calcular el porcentaje de citas por día
+    for dia, cantidad in conteo_dias.items():
+        porcentaje_por_dia[dia] = round((cantidad / total_citas) * 100, 2)
+        
+    dia = list(porcentaje_por_dia.keys()) # Obtener los días en el orden correcto
+    porcentajes_dia = list(porcentaje_por_dia.values()) # Obtener los porcentajes en el orden correcto
+    try:
+      # Crear el gráfico de barras con matplotlib
+        plt.figure(figsize=(5, 3))
+        plt.bar(dia, porcentajes_dia, color='skyblue', width=0.5)
+        plt.xlabel('dia')
+        plt.ylabel('Porcentaje (%)')
+        plt.title('Porcentaje de Citas por día')
+        plt.ylim(0, 100)  # Asegurar que el eje Y vaya de 0 a 100
+        
+        # plt.pie(porcentajes, labels=meses, autopct='%1.1f%%', startangle=140)
+        
+        # Guardar el gráfico en un buffer de memoria
+        buffer = io.BytesIO()
+        plt.savefig(buffer, format='png')
+        buffer.seek(0)
+        grafico_porcentaje_dias = base64.b64encode(buffer.read()).decode('utf-8')
+        plt.close()
+    except:
+      print('Ocurrió un error al crear el gráfico.')
+    
+    context = {
+        'conteo_meses': conteo_meses,
+        'conteo_dias': conteo_dias,
+        'total_citas': total_citas,
+        'porcentaje_por_mes': porcentaje_por_mes,
+        'porcentaje_por_dia': porcentaje_por_dia,
+        'grafico_porcentaje_meses': f'data:image/png;base64,{grafico_porcentaje_meses}',
+        'grafico_porcentaje_dias': f'data:image/png;base64,{grafico_porcentaje_dias}',
+    }
     return render(request, 'balance/citas_por_mes.html', context)
+
---
+++ pruebas.py
@@ -0,0 +1,3 @@
+total = [i for i in range(0, 100, 2)]
+
+print(total)
